3cdb3f842c885dc889ceaa6a10453e5b3aa5998d
williamsama44 <williamsama444@gmail.com>
2025-01-23 13:01:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "vaniakostadinova15513@gmail.com";
-const pass = "xmfloskqejlzktyb";
+//const pass = "xmfloskqejlzktyb";
 // 11th oct
 
 // Middleware
